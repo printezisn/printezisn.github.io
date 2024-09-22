959a0ead0bfe542cf288dcb7005ffc747a02c5fb
printezisn <printezisn@gmail.com>
2024-09-22 18:12:47 +0300

    Add assetpack

---
+++ assetpack.js
@@ -0,0 +1,32 @@
+import { AssetPack } from '@assetpack/core';
+import { json } from '@assetpack/core/json';
+import { compress } from '@assetpack/core/image';
+import { pixiManifest } from '@assetpack/core/manifest';
+import { texturePacker } from '@assetpack/core/texture-packer';
+import { webfont } from '@assetpack/core/webfont';
+import { audio } from '@assetpack/core/ffmpeg';
+import { cacheBuster } from '@assetpack/core/cache-buster';
+import { texturePackerCacheBuster } from '@assetpack/core/texture-packer';
+
+// eslint-disable-next-line
+const game = process.argv[2];
+
+const assetpack = new AssetPack({
+  entry: `./src/assets/games/${game}`,
+  output: `./public/games/${game}/assets`,
+  pipes: [
+    compress({
+      webp: { quality: 100, alphaQuality: 100 },
+      avif: { quality: 100, alphaQuality: 100 },
+    }),
+    pixiManifest(),
+    texturePacker(),
+    webfont(),
+    audio(),
+    json(),
+    cacheBuster(),
+    texturePackerCacheBuster(),
+  ],
+});
+
+assetpack.run();
