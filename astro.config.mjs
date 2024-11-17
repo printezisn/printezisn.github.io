8883a54acd4eadeec9def0df8643ba5ffaf95da7
printezisn <printezisn@gmail.com>
2024-11-17 13:50:22 +0200

    Copy couples run assets dynamically from the package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import fs from 'fs';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const gamePackages = Object.keys(
+  JSON.parse(fs.readFileSync('./package.json')).dependencies,
+).filter((game) => game.startsWith('@printezisn/games-'));
+
+const directoriesToCopy = [];
+gamePackages.forEach((game) => {
+  const name = game.substring('@printezisn/games-'.length);
+  directoriesToCopy.push(
+    {
+      src: `node_modules/${game}/dist/images/*`,
+      dest: `games/${name}`,
+    },
+    {
+      src: `node_modules/${game}/dist/assets`,
+      dest: `games/${name}`,
+    },
+  );
+});
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +30,11 @@ export default defineConfig({
       lastmod: new Date(),
     }),
   ],
+  vite: {
+    plugins: [
+      viteStaticCopy({
+        targets: directoriesToCopy,
+      }),
+    ],
+  },
 });
