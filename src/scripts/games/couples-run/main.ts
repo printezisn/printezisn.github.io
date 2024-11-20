8e42806208b23bb2fe44af2ef75f15dfe029d157
printezisn <printezisn@gmail.com>
2024-11-20 19:59:46 +0200

    Add version 1.3.0 of couples run

--- src/scripts/games/couples-run/main.ts
+++ src/scripts/games/couples-run/main.ts
@@ -2,4 +2,7 @@ import '../../base';
 import '../../../styles/games/couples-run/main.scss';
 import renderGame from '@printezisn/games-couples-run';
 
-renderGame({ assetsBasePath: '/games/couples-run/assets' });
+renderGame({
+  assetsBasePath: '/games/couples-run/assets',
+  creditsUrl: '/games/couples-run/credits',
+});
