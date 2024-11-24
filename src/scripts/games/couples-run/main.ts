576cf03d5b337b155f291ff89c12a2e869693d99
printezisn <printezisn@gmail.com>
2024-11-24 03:33:18 +0200

    Update couples run and add privacy policy

--- src/scripts/games/couples-run/main.ts
+++ src/scripts/games/couples-run/main.ts
@@ -5,4 +5,5 @@ import renderGame from '@printezisn/games-couples-run';
 renderGame({
   assetsBasePath: '/games/couples-run/assets',
   creditsUrl: '/games/couples-run/credits',
+  privacyPolicyUrl: '/games/couples-run/privacy-policy',
 });
