a19880808c0154820f51ec0fdd50470600d6381b
printezisn <printezisn@gmail.com>
2025-03-18 09:50:10 +0200

    Remove unnecessary image sizes

--- src/helpers/image-helper.ts
+++ src/helpers/image-helper.ts
@@ -1,8 +1,1 @@
-export const IMAGE_WIDTHS = [768, 1024, 1280, 1920, 2500];
-export const IMAGE_SIZES = IMAGE_WIDTHS.map((w, i) => {
-  if (i === IMAGE_WIDTHS.length - 1) {
-    return `${w}px`;
-  }
-
-  return `(max-width: ${w}px) ${w}px`;
-}).join(', ');
+export const IMAGE_WIDTHS = [768, 1280, 1920];
